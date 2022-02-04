1408a9351c1366b32a48c673a9283a77770f1bf7
변희성 <77eric2naver.com>
2022-02-04 11:00:09 +0900

    folder init

--- RTC/src/main/java/com/ssafy/rtc/controller/MainController.java
+++ RTC/src/main/java/com/ssafy/rtc/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.ssafy.rtc;
+package com.ssafy.rtc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
