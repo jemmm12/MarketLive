4ffa98bf57d81e8b0020a595d988b7e839d97132
asd2456542 <asd2456542@naver.com>
2022-02-17 23:23:09 +0900

    홈화면 유저 썸네일

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,28 +2,27 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Row, Spinner } from "react-bootstrap";
-import './Home.css'
-
+import "./Home.css";
 
 function Home() {
   const navigate = useNavigate();
 
-  const [broads, setBroads] = useState('')
-  const [myNickname, setMyNickname] = useState('')
-  const [myId, setMyId] = useState('')
+  const [broads, setBroads] = useState("");
+  const [myNickname, setMyNickname] = useState("");
+  const [myId, setMyId] = useState("");
 
   useEffect(() => {
     if (localStorage.jwt) {
       axios({
         method: "get",
         // url: "/auth/falsify/",
-        url: '/user/mypage/',
+        url: "/user/mypage/",
         headers: { Authorization: localStorage.getItem("jwt") },
       })
         .then((res) => {
           console.log(res);
-          setMyNickname(res.data.nickname)
-          setMyId(res.data.userid)
+          setMyNickname(res.data.nickname);
+          setMyId(res.data.userid);
         })
         .catch((err) => {
           console.log(err);
@@ -33,36 +32,48 @@ function Home() {
         });
     }
     axios({
-      method: 'get',
-      url: '/room/all',
+      method: "get",
+      url: "/room/all",
     })
-    .then(res => {
-      console.log(res)
-      setBroads(res.data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .then((res) => {
+        console.log("room List: ", res);
+        setBroads(res.data);
+        // res.data.map((room) => {
+        //   console.log("userid: ", room.userid);
+        //   axios({
+        //     method: "get",
+        //     url: "/user/thumbnail/" + room.userid,
+        //   })
+        //     .then((response) => {
+        //       console.log(response);
+        //       setBroads(...broads, response.data);
+        //     })
+        //     .catch((err) => {
+        //       console.log("error: ", err);
+        //     });
+        // });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   const onProfile = (e) => {
-    navigate("/profile/" + e)
+    navigate("/profile/" + e);
     // console.log(e)
   };
 
   const onBroad = (e) => {
-    console.log(e)
-    if (localStorage.getItem("jwt")){
-      navigate(`/watch/${e.userid}/${myId}/${myNickname}`)
+    console.log(e);
+    if (localStorage.getItem("jwt")) {
+      navigate(`/watch/${e.userid}/${myId}/${myNickname}`);
     } else {
-      alert('로그인이 필요합니다.')
-      navigate('/login')
+      alert("로그인이 필요합니다.");
+      navigate("/login");
     }
-    
-  }
+  };
 
-
-  if (broads === ''){
+  if (broads === "") {
     return (
       <div>
         <img
@@ -86,7 +97,7 @@ function Home() {
         <br /> <br /> <br /> <br />
         <Spinner animation="border" className="d-flex mx-auto" />
       </div>
-    )
+    );
   }
 
   return (
@@ -110,54 +121,73 @@ function Home() {
         className="d-flex mx-auto d-sm-none"
       />
       <br /> <br />
-      <div style={{ width: "90%", maxWidth:"1200px" }} className="mx-auto">
+      <div style={{ width: "90%", maxWidth: "1200px" }} className="mx-auto">
         <Row xs={1} sm={2} md={3} lg={4} className="">
           {broads.map((broad) => (
             <div key={broad.userid}>
               <div className="mb-4">
                 <div className="">
-                  {broad.thumbnail.includes('.') ? (
+                  {broad.thumbnail.includes(".") ? (
                     <img
                       src={broad.thumbnail}
                       alt=""
-                      style={{ height: "100%", width: "100%", cursor: "pointer" }}
-                      onClick={()=>{onBroad(broad)}}
+                      style={{
+                        height: "100%",
+                        width: "100%",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => {
+                        onBroad(broad);
+                      }}
                     />
                   ) : (
                     // <div  style={{ width: "100%"}}>
                     <img
-                      src='../img/thumbnail.png'
+                      src="../img/thumbnail.png"
                       // src='../img/home2.png'
                       alt=""
                       // style={{ height: "100%", width: "100%" }}
                       // width="320"
                       // height="180"
-                      style={{ width:"100%", cursor: "pointer"}}
-                      onClick={()=>{onBroad(broad)}}
+                      style={{ width: "100%", cursor: "pointer" }}
+                      onClick={() => {
+                        onBroad(broad);
+                      }}
                     />
                     // </div>
                   )}
                 </div>
                 <div className="d-flex mt-2">
                   <img
-                    src="../img/user.png"
+                    src={"/user/thumbnail/" + broad.userid}
                     alt=""
-                    style={{ width: "50px", height: "50px", cursor: "pointer" }}
-                    onClick={()=>{onProfile(broad.nickname)}}
+                    style={{
+                      width: "50px",
+                      height: "50px",
+                      cursor: "pointer",
+                      borderRadius: "70%",
+                    }}
+                    onClick={() => {
+                      onProfile(broad.nickname);
+                    }}
                   />
                   <div className="ms-1">
                     <div>
                       <span
                         className="fw-bold"
-                        onClick={()=>{onBroad(broad.nickname)}}
+                        onClick={() => {
+                          onBroad(broad.nickname);
+                        }}
                         style={{ cursor: "pointer" }}
                       >
                         {broad.title}
                       </span>
                     </div>
                     <div>
-                      <span 
-                        onClick={()=>{onProfile(broad.nickname)}} 
+                      <span
+                        onClick={() => {
+                          onProfile(broad.nickname);
+                        }}
                         style={{ cursor: "pointer" }}
                       >
                         {broad.nickname}
@@ -168,7 +198,7 @@ function Home() {
               </div>
             </div>
           ))}
-        </Row> 
+        </Row>
       </div>
     </div>
   );
